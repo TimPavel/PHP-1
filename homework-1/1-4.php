e5aed78a1357c7aeffe362d64889b02e988568a4
TimPavel <timpn@yandex.ru>
2018-10-06 18:38:06 +0300

    Done homework-1

---
+++ homework-1/1-4.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+$h1 = "<h1>";
+$title = "Телефон Nokia 6230";
+$currentYear = 2018;
+
+echo "<!DOCTYPE html>";
+echo "<html lang=\"en\">";
+echo "<head>";
+echo "<meta charset=\"UTF-8\">";
+echo "<title>Главная</title>";
+echo "</head>";
+echo "<body>";
+echo "$h1 $title </h1>";
+echo "<h3><img alt=\"здесь должна быть картинка\"></h3>";
+echo "<h3>Описание:</h3>";
+echo "<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+			Doloremque dolores magni fugit eligendi ratione ab.
+	  </p>";
+echo "<h3>Характеристики товара:</h3>";
+echo "<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+			Doloremque dolores magni fugit eligendi ratione ab.
+		</p>";
+echo "<h3>Подробное описание товара:</h3>";
+echo "<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+			Odit dignissimos cum asperiores deleniti omnis! Hic obcaecati, cumque nemo? <br>
+			Iste sunt explicabo eaque delectus ea nulla aut atque iure rem ipsam quo quis velit hic libero corporis <br>
+			numquam voluptate saepe, cum, eligendi temporibus officia esse, iusto ab aspernatur! Distinctio, quia, eum.
+	  </p>";
+echo "<hr>";
+echo "<footer class=\"footer\">&copy; Все права разрешены $currentYear.</footer>";
+echo "</body>";
+echo "</html>";
+?>
---
+++ homework-1/1-5.php
@@ -0,0 +1,16 @@
+﻿<?php
+$a = 2;
+$b = 4;
+
+echo "a равно $a<br>";
+echo "b равно $b<br>";
+
+$a = $a - $b;
+$b = $b + $a;
+$a = $b - $a;
+
+echo "<hr>";
+
+echo "a равно $a<br>";
+echo "b равно $b<br>";
+?>
